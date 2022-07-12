75410d2f6cdd526fa09a7481c06186cfe740b175
yesswssdlh <3278195170@qq.com>
2022-07-12 15:01:30 +0800

    优化

--- src/views/home/components/ArticleList.vue
+++ src/views/home/components/ArticleList.vue
@@ -24,7 +24,8 @@ export default {
     return {
       list: [], // 文章列表数组
       loading: false, // 底部加载状态
-      finished: false// 底部完成状态
+      finished: false, // 底部完成状态
+      theTime: (new Date()).getTime()// 用于分页
     }
   },
   props: {
@@ -39,14 +40,33 @@ export default {
   async created () {
     const res = await getAllArticleListAPI({
       channel_id: this.channelId, // 先默认请求推荐频道数据
-      timestamp: (new Date()).getTime()
+      timestamp: this.theTime
     })
+    // 实现分页
+    // pre_timestamp表示下一段开头的那篇文章时间戳
+    // 第一次 系统时间（timestamp）->后台 返回0-9条数据->携带第10条pre_timestamp值返回
+    // 第二次 （timestamp）-上一次pre_timestamp（代表告诉后台，从指定的时间戳再往后找10个）
+    // 10-19条，20条pre_timestampz返回
+    // ...
     this.list = res.data.data.results
+    this.theTime = res.data.data.pre_timestamp
   },
   methods: {
     // 底部加载的事件方法
-    onLoad () {
-
+    async onLoad () {
+      const res = await getAllArticleListAPI({
+        channel_id: this.channelId, // 先默认请求推荐频道数据
+        timestamp: this.theTime
+      })
+      // 如果剩下来的数据没有了，就不要进行下面的操作了，直接结束加载
+      if (res.data.data.results.length === 0) {
+        this.finished = true
+        return
+      }
+      // 这里是将前10个数据和后面请求获得的数据结合到一个数组中
+      this.list = [...this.list, ...res.data.data.results]
+      this.theTime = res.data.data.pre_timestamp
+      this.loading = false // 如果不关闭，底部一直是加载中状态，下次触底会再触发onload
     }
   }
 }
